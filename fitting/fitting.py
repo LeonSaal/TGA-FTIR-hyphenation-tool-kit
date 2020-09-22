14d4c08fe28e3cb1fe109a3cd843abc91addfec6
LeonSaal <68274160+LeonSaal@users.noreply.github.com>
2020-09-22 17:50:12 +0200

    Update fitting.py
    Update of fitting() to receive individual initial values as well as bounds.  Addition of fits() for multiple class TG_IR objects

--- fitting/fitting.py
+++ fitting/fitting.py
@@ -30,8 +30,8 @@ def baseline_als(y, lam=1e6, p=0.01, niter=10): #https://stackoverflow.com/quest
         w = p * (y > z) + (1-p) * (y < z)
     return z
 
-def fitting(TG_IR,centers,labels,func,tol_center=BOUNDS.getfloat('delta_tm'),max_hwhm=BOUNDS.getfloat('hwhm_max'),y_axis='orig',plot=False,save=True,init_offs=[0,0,0]):
-    gases=list(centers.columns.values)
+def fitting(TG_IR,presets,func=multi_gauss,y_axis='orig',plot=False,save=True):
+    gases=[key for key in presets]
     if ('co2' in gases) and ('co' in gases):
         gases.remove('co2')
         gases.insert(0,'co2')
@@ -39,91 +39,56 @@ def fitting(TG_IR,centers,labels,func,tol_center=BOUNDS.getfloat('delta_tm'),max
     ref_mass=TG_IR.info['reference_mass']
     
     #initializing output DataFrame
-    peaks=pd.DataFrame(columns=['center','height','hwhm','area','mmol','mmol_per_mg'],index=[group+'_'+gas.upper() for gas in gases for group in labels[gas].dropna()]+[gas.upper() for gas in gases])
+    peaks=pd.DataFrame(columns=['center','height','hwhm','area','mmol','mmol_per_mg'],index=[group+'_'+gas.upper() for gas in gases for group in presets[gas].index]+[gas.upper() for gas in gases])
     sumsqerr=pd.DataFrame(index=[TG_IR.info['name']],columns=gases)
 
     #cycling through gases
     FTIR=TG_IR.ir.copy()
     for gas in gases:
-        #print(gas)
-        params=pd.DataFrame(columns=['min_center','init_center','max_center','min_height','init_height','max_height','min_hwhm','init_hwhm','max_hwhm'],index=[group+'_'+gas.upper() for group in labels[gas].dropna()])
-
         if gas=='h2o':
             FTIR[gas]-=baseline_als(FTIR[gas])
             
         #molar desorption
         tot_area=np.sum(TG_IR.ir[gas])
         if gas == 'h2o':
-            tot_area=np.sum(TG_IR.ir[gas][TG_IR.ir['Ts']>TG_IR.info['dry_temp']])
+            tot_area=np.sum(TG_IR.ir[gas][TG_IR.ir['sample_temp']>TG_IR.info['dry_temp']])
         tot_mol=(tot_area-TG_IR.linreg['intercept'][gas])/TG_IR.linreg['slope'][gas]
         peaks['area'][gas.upper()]=tot_area
         peaks['mmol'][gas.upper()]=tot_mol
         peaks['mmol_per_mg'][gas.upper()]=tot_mol/TG_IR.info[ref_mass]
         
-
-        
         if y_axis=='rel':
             FTIR.update(FTIR[gas]/tot_area*tot_mol)
         
         #initial guesses
-        init_centers=np.array(centers[gas].dropna().astype(float))+init_offs[0]
-        num_curves=len(init_centers)
-        max_heigth=max(FTIR[gas])
-        init_heights=(BOUNDS.getfloat('height_0')+init_offs[1])*max_heigth*np.ones(num_curves)
-        init_hwhms=(BOUNDS.getfloat('hwhm_0')+init_offs[2])*max_hwhm*np.ones(num_curves)
+        params=['height_0', 'hwhm_0', 'center_min', 'hwhm_min','height_min', 'center_max','hwhm_max', 'height_max']
+        vals=[BOUNDS.getfloat('height_0')*max(FTIR[gas]) if BOUNDS['height_max'] == 'max' else (BOUNDS.getfloat('height_0')* BOUNDS.getfloat('height_max')), 
+              BOUNDS.getfloat('hwhm_0')*BOUNDS.getfloat('hwhm_max'), 
+              pd.Series(presets[gas].loc[:,'center_0']-BOUNDS.getfloat('tol_center')), 
+              BOUNDS.getfloat('hwhm_min'),
+              BOUNDS.getfloat('height_min'), 
+              pd.Series(presets[gas].loc[:,'center_0']+BOUNDS.getfloat('tol_center')), 
+              BOUNDS.getfloat('hwhm_max'), 
+              max(FTIR[gas]) if BOUNDS['height_max'] == 'max' else BOUNDS.getfloat('height_max')]
+        infill=dict(zip(params,vals))
+        presets[gas]=presets[gas].fillna(infill)
         
-        #lower bounds for fit
-        min_heights=0*np.ones(num_curves)
-        min_hwhms=0*np.ones(num_curves)
-        min_centers=init_centers-tol_center
-        
-        #upper bounds for fit
-        max_heights=max_heigth*np.ones(num_curves)
-        max_hwhms=max_hwhm*np.ones(num_curves)
-        max_centers=init_centers+tol_center
-        
-        #print(min_centers,init_centers,max_centers)
-        params['init_center']=init_centers
-        params['init_height']=init_heights
-        params['init_hwhm']=init_hwhms
-        params['min_center']=min_centers
-        params['min_height']=min_heights
-        params['min_hwhm']=min_hwhms
-        params['max_center']=max_centers
-        params['max_height']=max_heights
-        params['max_hwhm']=max_hwhms
-        #assumptions by figueiredo
-        a=True
-        if (gas=='co' and a==True):
-            try:
-                loc=labels[gas][labels[gas]=='anhydrides'].index[0]
-
-                init_heights[loc]=peaks['height']['anhydrides_CO2']/TG_IR.linreg['slope']['co2']*TG_IR.linreg['slope']['co']
-                min_heights[loc]=init_heights[loc]*.9
-                max_heights[loc]=init_heights[loc]*1.1
-
-                init_hwhms[loc]=peaks['hwhm']['anhydrides_CO2']
-                min_hwhms[loc]=init_hwhms[loc]*.9
-                max_hwhms[loc]=init_hwhms[loc]*1.1
-            except:
-                pass
-            
-            
         #guesses and bounds
-        init_guess=np.concatenate((init_heights,init_centers,init_hwhms))
-        min_param=np.concatenate((min_heights,min_centers,min_hwhms))
-        max_param=np.concatenate((max_heights,max_centers,max_hwhms))
-    
+        params_0=np.concatenate(([presets[gas].loc[:,key+'_0'] for key in ['height', 'center', 'hwhm']])) 
+        params_min=np.concatenate(([presets[gas].loc[:,key+'_min'] for key in ['height', 'center', 'hwhm']])) 
+        params_max=np.concatenate(([presets[gas].loc[:,key+'_max'] for key in ['height', 'center', 'hwhm']])) 
+        
         #actual fitting
-        x=FTIR['Ts']
+        x=FTIR['sample_temp']
         try:
-            popt,pcov=sp.optimize.curve_fit(func,x,FTIR[gas],p0=init_guess,bounds=(min_param,max_param))
+            popt,pcov=sp.optimize.curve_fit(func,x,FTIR[gas],p0=params_0,bounds=(params_min,params_max))
         except:
             print('Failed to fit {} signal'.format(gas.upper()))
             break
         #return values
+        num_curves=len(presets[gas])
         for i in range(num_curves):
-            group=labels[gas][i]+'_'+gas.upper()
+            group=presets[gas].index[i]+'_'+gas.upper()
             peaks['height'][group]=popt[i]
             peaks['center'][group]=popt[i+num_curves]
             peaks['hwhm'][group]=popt[i+2*num_curves]
@@ -141,7 +106,7 @@ def fitting(TG_IR,centers,labels,func,tol_center=BOUNDS.getfloat('delta_tm'),max
         fit=multi_gauss(x,*popt)
         diff=data-fit
         sumsqerr[gas][TG_IR.info['name']]=np.sum(np.power(diff,2))
-        profiles['Ts']=x
+        profiles['sample_temp']=x
         profiles['data']=data
         profiles['fit']=fit
         profiles['diff']=diff
@@ -160,17 +125,17 @@ def fitting(TG_IR,centers,labels,func,tol_center=BOUNDS.getfloat('delta_tm'),max
             fitting.plot(x,fit,label='fit',lw=2,zorder=num_curves+2)
         for i in range(0,num_curves):
             y=gaussian(x,popt[i],popt[i+num_curves],popt[i+2*num_curves])
-            profiles[labels[gas][i]]=y
+            profiles[presets[gas].index[i]]=y
             if plot:
-                fitting.text(popt[num_curves+i],popt[i],labels[gas][i],zorder=num_curves+3+i)
+                fitting.text(popt[num_curves+i],popt[i],presets[gas].index[i],zorder=num_curves+3+i)
                 fitting.plot(x,y,linestyle='dashed',zorder=i)
         if plot:
             fitting.legend()
-            fitting.set_xlabel('{} {} ${}$'.format(UNITS['ts'], SEP, UNITS['ts']))
+            fitting.set_xlabel('{} {} ${}$'.format(UNITS['sample_temp'], SEP, UNITS['sample_temp']))
             if y_axis=='orig':
                 fitting.set_ylabel('{} {} ${}$'.format(get_label(gas), SEP, UNITS['ir']))
             elif y_axis=='rel':
-                fitting.set_ylabel('{} {} ${}\,{}^{{-1}}\,{}^{{-1}}$'.format(get_label(gas), SEP, UNITS['molar_amount'], UNITS['mass'], UNITS['time']))
+                fitting.set_ylabel('{} {} ${}\,{}^{{-1}}\,{}^{{-1}}$'.format(get_label(gas), SEP, UNITS['molar_amount'], UNITS['sample_mass'], UNITS['time']))
 
             #mark center on x-axis
             fitting.scatter(popt[num_curves:2*num_curves],np.zeros(num_curves),marker=7,color='k',s=100,zorder=num_curves+3)
@@ -181,7 +146,7 @@ def fitting(TG_IR,centers,labels,func,tol_center=BOUNDS.getfloat('delta_tm'),max
             error.text(0,abs_max,'SQERR: {:.2e}'.format(sumsqerr[gas][TG_IR.info['name']]))#,'SQERR: '+'%.2E'% Decimal(sumsqerr[gas][TG_IR.info['name']]),va='bottom')
             error.plot(x,diff)
             error.hlines(0,min(x),max(x),ls='dashed')
-            error.set_xlabel('{} {} ${}$'.format(UNITS['ts'], SEP, UNITS['ts']))
+            error.set_xlabel('{} {} ${}$'.format(UNITS['sample_temp'], SEP, UNITS['sample_temp']))
             error.set_ylabel('error {} ${}$'.format(SEP, UNITS['ir']))
             error.set_ylim(-abs_max,abs_max)
             plt.show()
@@ -190,13 +155,100 @@ def fitting(TG_IR,centers,labels,func,tol_center=BOUNDS.getfloat('delta_tm'),max
             try:
                 with pd.ExcelWriter(TG_IR.info['name']+y_axis+'.xlsx',engine='openpyxl', mode='a') as writer:
                     profiles.to_excel(writer,sheet_name=gas)
-                    params.to_excel(writer,sheet_name=gas+'_param')
+                    presets[gas].to_excel(writer,sheet_name=gas+'_param')
             except:
                 with pd.ExcelWriter(TG_IR.info['name']+y_axis+'.xlsx',engine='openpyxl') as writer:
                     profiles.to_excel(writer,sheet_name=gas)
-                    params.to_excel(writer,sheet_name=gas+'_param')
+                    presets[gas].to_excel(writer,sheet_name=gas+'_param')
 
     if save:                
         with pd.ExcelWriter(TG_IR.info['name']+y_axis+'.xlsx',engine='openpyxl', mode='a') as writer:
                 peaks.astype(float).to_excel(writer,sheet_name='summary')
     return peaks.astype(float),sumsqerr
+
+def fits(*TG_IR,reference,**kwargs):
+    if T_max==None:
+        T_max=min([max(obj.tga['sample_temp']) for obj in TG_IR])
+
+    elif T_max>min([max(obj.tga['sample_temp']) for obj in TG_IR]):
+        T_max=T_max=min([max(obj.tga['sample_temp']) for obj in TG_IR])
+        print('$T_{max}$ exceeds maximum temperature of data')
+
+     ###extracting initial values for fitting from reference
+    presets=dict()
+    references=pd.read_excel(os.path.join(PATHS['dir_home'],'Fitting parameter.xlsx'),index_col=0,header=None,sheet_name=None)
+    gases=list(set(references['center_0'].loc['gas']))
+    cols=[key for key in references]
+    
+    for gas in gases:
+        index=[references['center_0'].loc['group',i] for i in references['center_0'].columns if (references['center_0'].loc['gas',i]==gas)]
+        data=pd.DataFrame(index=index)
+        for key in references:
+            #print(references[key])
+            references[key]=references[key]#.dropna(axis=0,thresh=1).dropna(axis=1,thresh=1)
+            data[key]=pd.DataFrame(references[key].loc[reference,:][references[key].loc['gas',:]==gas].T.values,index=index,columns=[key])#.dropna(axis=1)
+        presets[gas]=data.dropna(axis=0,how='all')      
+            
+    
+    #initializing of output DataFrames
+    col_labels=[group+'_'+gas.upper() for gas in gases for group in presets[gas].dropna()]+[gas.upper() for gas in gases]
+    err=pd.DataFrame(columns=gases)
+    names=['center','height','hwhm','area','mmol','mmol_per_mg']
+    res=dict()
+    for name in names:
+        res[name]=pd.DataFrame(columns=col_labels)
+    
+    #make subdirectory to save data
+    if save==True:
+        path=os.path.join(PATHS['dir_fitting'],general.time()+reference+'_'+'_'.join(list(set([str(obj.info['sample']) for obj in TG_IR]))))
+        os.makedirs(path)
+        os.chdir(path)
+    
+    #cycling through samples
+    for obj in TG_IR:
+        #fitting of the sample and calculating the amount of functional groups
+        if T_max!=None:
+            temp=copy.deepcopy(obj)
+            temp.tga=temp.tga[temp.tga['sample_temp']<T_max]
+            temp.ir=temp.ir[temp.ir['sample_temp']<T_max]
+        else:
+            temp=obj
+        peaks,sumsqerr=fit.fitting(temp,presets,**kwargs)
+
+        #writing data to output DataFrames
+        for key in res:
+            res[key]=res[key].append(peaks[key].rename(obj.info['name']).T)  
+        err=err.append(sumsqerr)
+
+    # calculate statistical values
+    dm=1e-6
+    for key in res:
+        samples=list(set([get_label(re.search('(?<=_)\d{5}(?=_\d{2,3})',index).group()) for index in res[key].index]))
+        stddev=pd.DataFrame(columns=res[key].columns,index=[sample+'_stddev' for sample in samples])
+        mean=pd.DataFrame(columns=res[key].columns,index=[sample+'_mean' for sample in samples])
+        for sample in samples:
+            for column in res[key].columns:
+                gas=column[column.rfind('_')+1:].lower()
+                indices=[index for index in res[key].index if get_label(re.search('(?<=_)\d{5}(?=_\d{2,3})',index).group())==sample]
+                subset=res[key][column].loc[indices]
+                if key=='mmol_per_mg':
+                    mmol=res['mmol'][gas.upper()].loc[indices]#res['mmol'][column].loc[indices]
+                    g=mmol/subset
+                    lod=TG_IR[0].stats['x_NG'][gas]
+                    dmmolg_i=np.power(np.power(lod/mmol,2)+np.power(dm/g,2),0.5)*subset
+                    dmmol=np.power(np.sum(np.power(dmmolg_i,2)),0.5)
+                    stddev[column][sample+'_stddev']=dmmol
+                else:
+                    stddev[column][sample+'_stddev']=np.std(subset)
+                mean[column][sample+'_mean']=np.mean(subset)
+        res[key]=res[key].append(mean)
+        res[key]=res[key].append(stddev)        
+    
+    #exporting data
+    if save==True:
+        with pd.ExcelWriter('summary.xlsx') as writer:
+            for key in res:
+                res[key].dropna(axis=1).to_excel(writer,sheet_name=key)
+            err.to_excel(writer,sheet_name='sum_squerr')
+        os.chdir(PATHS['dir_home'])
+    return res
