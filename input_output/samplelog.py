4769882854abe391783c30f39efa8dc1ed2ffa72
LeonSaal <68274160+LeonSaal@users.noreply.github.com>
2020-09-23 12:28:02 +0200

    Add files via upload
    Minor fixes

--- input_output/samplelog.py
+++ input_output/samplelog.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from ..config import PATHS
 
-def samplelog(info,overwrite=False):
+def samplelog(info=None,overwrite=False):
     path=os.path.join(PATHS['dir_home'],'Samplelog.xlsx')
     
     try:
@@ -12,20 +12,21 @@ def samplelog(info,overwrite=False):
     except:
         print('> Samplelog konnte nicht gefunden werden. Leerer Samplelog wird angelegt.')
         samplelog=pd.DataFrame(columns=['alias','reference'])
-
-    name=info['name']
-    data=pd.DataFrame.from_dict(info,orient='index',columns=[name]).T.drop(['name'],1)
-    
-    for key in data.columns:
-        if key not in samplelog.columns:
-            samplelog[key]=np.nan
-    if name in samplelog.index:
-        if overwrite==False:
-            samplelog=samplelog.fillna(data)
+        
+    if info!=None:
+        name=info['name']
+        data=pd.DataFrame.from_dict(info,orient='index',columns=[name]).T.drop(['name'],1)
+        
+        for key in data.columns:
+            if key not in samplelog.columns:
+                samplelog[key]=np.nan
+        if name in samplelog.index:
+            if overwrite==False:
+                samplelog=samplelog.fillna(data)
+            else:
+                samplelog.loc[[name]]=data
         else:
-            samplelog.loc[[name]]=data
-    else:
-        samplelog=samplelog.append(data)
-
-    samplelog.to_excel(os.path.join(PATHS['dir_home'],'Samplelog.xlsx'))#,sep=';',decimal=',',thousands='.')
+            samplelog=samplelog.append(data)
+    
+        samplelog.to_excel(os.path.join(PATHS['dir_home'],'Samplelog.xlsx'))
     return samplelog
