1bd40b3fa84a6aff1a89452a15dacfc4a472f40a
danield5732 <nochschneller@onlinehome.de>
2021-02-09 11:09:13 +0100

    Check if supplied 'dir_data' is valid

--- TGA_FTIR_tools/config.py
+++ TGA_FTIR_tools/config.py
@@ -12,9 +12,11 @@ if PATHS['dir_home']=='':
     cwd=os.getcwd().replace(os.sep,os.altsep)
     print('No dir_home path was supplied in {}. dir_home was set to \'{}\''.format(file,cwd))
     config['paths']['dir_home']=cwd
-if PATHS['dir_data']=='':
-    print('\nNo dir_data path was supplied in {}.'.format(file))
+if PATHS['dir_data']=='' or os.path.exists(PATHS['dir_data'])==False:
+    print('\nNo valid dir_data path was supplied in \'{}\'.'.format(file))
     config['paths']['dir_data']=input('Supply directory of Data:').replace(os.sep,os.altsep)
+    if os.path.exists(config['paths']['dir_data'])==False:
+        print('\n!!! Supplied directory does not exist. Revise path in \'settings.ini\' prior to continue. !!!\n')
 
 with open(file, 'w') as configfile:
     config.write(configfile)
