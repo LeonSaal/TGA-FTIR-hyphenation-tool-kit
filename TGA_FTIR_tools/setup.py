a9bb43747738e76477b50d6be841c78c6b4e5949
LeonSaal <68274160+LeonSaal@users.noreply.github.com>
2022-05-27 11:51:46 +0200

    removed packages keyword

--- TGA_FTIR_tools/setup.py
+++ TGA_FTIR_tools/setup.py
@@ -12,7 +12,6 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/BAMresearch/TGA-FTIR-hyphenation-tool-kit",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3.0",
