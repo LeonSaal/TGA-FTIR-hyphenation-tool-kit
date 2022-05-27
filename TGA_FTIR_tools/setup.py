08b2a167f4f224f9b8f053ce48b100b21975749c
LeonSaal <68274160+LeonSaal@users.noreply.github.com>
2022-05-27 10:08:03 +0200

    changed name

--- TGA_FTIR_tools/setup.py
+++ TGA_FTIR_tools/setup.py
@@ -4,19 +4,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="TGA-FTIR-hyphenation-tool-kit-LeonSaal",  # Replace with your own username
+    name="TGA-FTIR-hyphenation-tool-kit",  # Replace with your own username
     version="v2.2",
     author="Leon Saal",
-    author_email="leon.saal@posteo.de",
+    author_email="mail.leon.saal@gmail.com",
     description="A package for handling hyphenated TGA and FTIR data. Includes basic plotting as well as as advanced deconvolution of FTIR data.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/LeonSaal/TGA-FTIR-hyphenation-tool-kit",
+    url="https://github.com/BAMresearch/TGA-FTIR-hyphenation-tool-kit",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3.0",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
 )
